4eb830f918b33acfe6ead32083edd642cb2fce54
miro83kolev <miro83kolev@gmail.com>
2022-11-30 14:51:13 +0200

    update

--- basic_syntax/functions/nxnMatrix.js
+++ basic_syntax/functions/nxnMatrix.js
@@ -1,18 +1,34 @@
-function matrixN(num) {
-    function lines(a) {
- 
-        let thelineOne = a.toString().repeat(a + ' ').split('').join(' ');
- 
-        return thelineOne + '\n';
-    }
- 
-    function reps(ass) {
-        for (let i = 1; i <= ass; i++) {
-            return lines(ass).repeat(ass-1);
+function matrix(n){
+
+    let row = n
+    let col = n
+    let matrix = []
+    let num = n
+
+    for (let i = 0; i < row; i++) {
+        matrix.push([])
+        for (let j = 0; j < col; j++) {
+            matrix[i].push(num+ ' ')
         }
- 
     }
-    return lines(num) + reps(num);
+
+    return matrix.join('\n')
+    
 }
 
-console.log(matrixN(7))
+console.log(matrix(7))
+
+
+function matrix1(n) {
+        let m = n
+        let result = []
+        for(let i = 0; i < n; i++) {
+            result.push(new Array(m).fill(n))
+        }
+        result.join(' ')
+        let result1 = result.replace(/,/g, " ")
+
+        return result1
+
+    }
+console.log(matrix1(3))
