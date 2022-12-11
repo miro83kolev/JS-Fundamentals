f8a0d177eb0143ae9f80ed4444b3e82fa464399b
miro83kolev <miro83kolev@gmail.com>
2022-12-11 18:05:50 +0200

    update

---
+++ .vscode/obj and classes/town.js
@@ -0,0 +1,21 @@
+function town(input) {
+    let result = {}
+
+    for (let tokens of input) {
+        let info = tokens.split(' | ')
+        let townName = info[0]
+        let latitude = Number(info[1]).toFixed(2)
+        let longitude = Number(info[2]).toFixed(2)
+
+        result.town = townName
+        result.latitude = latitude
+        result.longitude = longitude
+
+        console.log(result)
+    }
+}
+
+town(['Sofia | 42.696552 | 23.32601',
+    'Beijing | 39.913818 | 116.363625']
+)
+town(['Plovdiv | 136.45 | 812.575'])
