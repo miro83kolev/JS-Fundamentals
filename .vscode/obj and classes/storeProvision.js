7772b33b4c814844c000c6222f4909fa4e4e3879
miro83kolev <miro83kolev@gmail.com>
2022-12-13 17:47:47 +0200

    update

---
+++ .vscode/obj and classes/storeProvision.js
@@ -0,0 +1,29 @@
+function storeProvision(availableStock, deliveredStock) {
+    let storedProducts = {}
+
+    for (let index = 0; index < availableStock.length; index += 2) {
+        let currentProduct = availableStock[index]
+        storedProducts[currentProduct] = Number(availableStock[index + 1])
+    }
+
+    for (let index = 0; index < deliveredStock.length; index += 2) {
+        let currentProduct = deliveredStock[index]
+
+        if (!storedProducts.hasOwnProperty(currentProduct)) {
+            storedProducts[currentProduct] = 0
+        }
+        storedProducts[currentProduct] += Number(deliveredStock[index + 1])
+    }
+
+    for (let product in storedProducts) {
+        console.log(`${product} -> ${storedProducts[product]}`)
+    }
+}
+
+storeProvision([
+    'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
+],
+    [
+        'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
+    ]
+)
